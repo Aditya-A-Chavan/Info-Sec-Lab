4d1d8abf90ed8d50e1a37c270952f6aff021f1cc
Aditya-A-Chavan <adityachavan271@gmail.com>
2024-08-07 12:38:36 +0530

    expt3

--- expt3/server.py
+++ expt3/server.py
@@ -1,1 +1,25 @@
 import socket   
+
+def start_server(HOST, PORT):
+    server_s = client_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_s.bind((HOST, PORT))
+    server_s.listen(1)
+
+    print(f"Server listening on port {HOST}:{PORT}")
+
+    while True:
+        client_s, addr = server_s.accept()
+
+        print(f"Connected to the following machine: {HOST}:{PORT}")
+
+        message = client_s.recv(1024).decode('utf-8')
+
+        if not message:
+            break
+
+        print(f"message recieved from client: {message}")
+
+
+
+if __name__ == "__main__":
+    start_server('0.0.0.0', 9186)
